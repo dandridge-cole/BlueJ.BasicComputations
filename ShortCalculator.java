d7377f37adec97cd35046d50f86a4222c4ad2787
Dandridge Cole <dandridgecole@yahoo.com>
2019-05-23 19:58:04 -0400

    Take 2

--- ShortCalculator.java
+++ ShortCalculator.java
@@ -2,4 +2,30 @@
 
 
 public class ShortCalculator {
+    public short sum(short num1, short num2){
+        short ans = (short)(num1+num2);
+        if (ans<0) ans *= (short)(-1);
+        return ans;
+    }
+        public short difference(short num1, short num2){
+        short ans = (short)(num1 - num2);
+        if (ans<0) ans *= (short)(-1);
+        return ans;
+    }
+        public short product(short num1, short num2){
+        short ans = (short)(num1*num2);
+        if (ans<0) ans *= (short)(-1);
+        return ans;
+    }
+        public short quotient(short num1, short num2){
+        short ans = (short)(num1/num2);
+        if (ans<0) ans *= (short)(-1);
+        return ans;
+    }
+        public short remainder(short num1, short num2){
+        short ans = (short)(num1%num2);
+        if (ans<0) ans *= (short)(-1);
+        return ans;
+    }
+
 }
